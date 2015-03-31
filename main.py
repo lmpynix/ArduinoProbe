c7ed92ef818fc56e9206020ce7cb8f4636ed4e07
lmpynix <lmpynix@gmail.com>
2015-03-31 12:27:00 -0500

    Cleaned up the code a bit.

--- main.py
+++ main.py
@@ -2,32 +2,36 @@
 
 from pyfirmata import Arduino as ard
 from pyfirmata import util
-import thread, os, time
+import threading, os, time
 VERSION = "0.0.1"
 
 probeaddrs = []
 probes = []
 
+
 def init():
-    probeaddrs.insert(0, raw_input("Enter the address of the probe with ID 0: "))
-    print("Attempting to connect to probe on ", probeaddrs[0], "...")
+    probeaddrs.insert(0, input('Enter the address of the probe with ID 0: '))
+    print('Attempting to connect to probe on ', probeaddrs[0], '...')
     try:
         probes.insert(0, ard(probeaddrs[0]))
     except:
-        print("Connection Failed.")
+        print('Connection Failed.')
         return 1
-    print("Connection Succeeded.  Device with ID 0 is connected.")
+    print('Connection Succeeded.  Device with ID 0 is connected.')
     return 0
-    
+
+
 def main():
-    print("LPyKit ArduinoProbe version ", VERSION)
+    print('LPyKit ArduinoProbe version ', VERSION)
     init()
 
-def blink(board, time):
-    probes[board].digital[13].write(1)
-    time.sleep(time)
-    probes[board].digital[13].write(0)
-    time.sleep(time)
 
-if __name__ == "__main__":
+def blink(board, freq):
+    probes[board].digital[13].write(1)
+    time.sleep(freq)
+    probes[board].digital[13].write(0)
+    time.sleep(freq)
+
+
+if __name__ == '__main__':
     main()
